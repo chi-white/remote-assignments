065ba51f8502b61ab817d92886a2d42f7bd0e05f
chi-white <king0209king0209@gmail.com>
2023-10-07 23:17:37 +0800

    add env.sample, png files, client&server js file.

---
+++ hw2/test_client.js
@@ -0,0 +1,102 @@
+apiurl = 'http://localhost:3000/users' ;
+
+/*------------------------------------check function-----------------------------------------------*/ 
+function Validname(name) {
+  const regex = /^[a-zA-Z0-9]+$/;
+  return regex.test(name);
+}
+
+function Validemail(email) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
+
+function Validpassword(password) {
+  const uppercaseRegex = /[A-Z]/;
+  const lowercaseRegex = /[a-z]/;
+  const numberRegex = /[0-9]/;
+  const symbolRegex = /[~`!@#$%^&*()_\-+={[}\]|:;"'<,>.?/|]/;
+
+  let typesCount = 0;
+
+  if (uppercaseRegex.test(password)) {
+    typesCount++;
+  }
+  if (lowercaseRegex.test(password)) {
+    typesCount++;
+  }
+  if (numberRegex.test(password)) {
+    typesCount++;
+  }
+  if (symbolRegex.test(password)) {
+    typesCount++;
+  }
+
+  return typesCount >= 3;
+}
+/*-------------------------------------Sign up----------------------------------------------------*/ 
+const data = {
+    name: "123",
+    email: "123@gmail.com",
+    password: "12abAB"
+  };
+  
+
+const Postrequest = {
+method: 'POST',
+headers: {
+    'Content-Type': 'application/json',
+    'Request-Date' : new Date().toUTCString()
+},
+body: JSON.stringify(data)
+};
+
+// send POST to http://localhost:3000/users 
+if (Validname(data.name) && Validemail(data.email) && Validpassword(data.password)){
+  fetch(apiurl, Postrequest) 
+  .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Problem in response');
+      }
+  })
+  .then(data => {
+      console.log('sucess response：', data);
+  })
+  .catch(error => {
+      console.error('no problem in response but catch error:', error);
+  });
+}else{
+  console.log("Invalid Input") ;
+}
+
+/*----------------------------------------------Query---------------------------------------------------------------------*/
+// const Getrequest = {
+//   method: 'GET',
+//   headers: {
+//       'Content-Type': 'application/json',
+//       'Request-Date' : new Date().toUTCString()
+//   },
+//   };
+ 
+// const userid = 12;  
+
+// // GET request
+// fetch(`${apiurl}?id=${userid}`, Getrequest)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Problem in response');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log('sucess response : ', data);
+//   })
+//   .catch(error => {
+//     console.error('no problem in response but catch error:', error);
+//   });
+
+
+
+  
---
+++ hw2/test_server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+require('dotenv').config() ;
+
+// Configure body-parser to parse JSON data
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'kimdb.clhgz7gmuaob.ap-southeast-2.rds.amazonaws.com',
+  user: process.env.user,
+  password: process.env.password,
+  database: 'assignment',
+});
+
+// Connect to the MySQL database
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+/*----------------------------Sign Up--------------------------------------------------------------------*/
+
+app.post('/users', (req, res) => {
+  const { name, email, password } = req.body;
+  const request_date = req.headers['request-date'];
+  const insertUserQuery = 'INSERT INTO user (name, email, password) VALUES (?, ?, ?)';
+
+  db.query(insertUserQuery, [name, email, password, request_date], (err, results) => {
+    if (err) {
+      console.error('Error inserting user: ' + err.stack);
+      if (err.code === 'ER_DUP_ENTRY'){
+        return res.status(409).json({ error: 'Email Already Exists' });
+      }else {
+        return res.status(400).json({ error: 'Client Error Response' });
+      }
+    }
+    const userId = results.insertId;
+    const user = {
+      id: userId,
+      name,
+      email,
+    };
+    // Return the user information in the response
+    res.status(200).json({ data: {user, request_date} });
+  });
+});
+
+/*----------------------------query--------------------------------------------------------------------*/
+// app.get('/users', (req, res) => {
+//   const {id} = req.query;
+//   if (isNaN(id) || id<= 0){
+//     return res.status(500).json({ error: "id" });
+//   }
+
+//   const request_date = req.headers['request-date'] ;
+
+//   // DB
+//   const selectUserQuery = 'SELECT id, name, email FROM user WHERE id = ?';
+//   db.query(selectUserQuery, [id, request_date], (err, results) => {
+//     if (err) {
+//       console.error('Error querying user: ' + err.stack);
+//       return res.status(400).json({ error: 'Client Error Response' });
+//     }
+//     if (results.length === 0) {
+//       console.log('User Not Existing');
+//       return res.status(403).json({ error: 'User Not Existing'});
+//     }
+
+//     const user = results[0];
+//     console.log(user);
+//     res.status(200).json({ data: {user, "request_date":request_date} });
+    
+//   });
+// });
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+
